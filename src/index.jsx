04a1dba9a21a04b3613e2b7c45c8623e432f7c88
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-09-12 08:42:58 -0300

    added support to redux and redux-thunk

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,17 @@ require('bootstrap');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './app';
+import reducers from './app/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
-    <App />
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
     , document.getElementById('react-app'));
