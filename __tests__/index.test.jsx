cffa82a995f5e3ba4a5abf1ef7a5ae14049d164c
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-09-22 08:10:40 -0300

    removed test modules for a while (version incompatibility)

--- __tests__/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import App from 'app';
-
-describe('App', () => {
-    it('renders without crashing', () => {
-        mount(<App />);
-    });
-});
