b556ee047a84d3bf0affe1724344ae391cfaac33
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-11-17 19:37:12 -0200

    arrow function improved

--- src/app/hoc/loadable.jsx
+++ src/app/hoc/loadable.jsx
@@ -1,11 +1,10 @@
 import L from 'react-loadable';
-import LoadingPage from '../component/loading-page'
+import LoadingPage from '../component/loading-page';
 
-const Loadable = (opts) => {
-  L({
-    LoadingComponent: LoadingComponent,
-    delay: 300,
-    ...opts
-  });
-}
+const Loadable = opts => L({
+  loading: LoadingPage,
+  delay: 300,
+  ...opts,
+});
+
 export default Loadable;
