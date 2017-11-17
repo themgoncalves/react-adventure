2635316d3201195c408c6b66053a64fe4069b4da
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-11-17 19:38:40 -0200

    webpack config updated -> fixed bug related to scss imported from node_modules

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,4 +1,5 @@
-'use strict';
+
+
 require('babel-polyfill');
 const webpack = require('webpack');
 const path = require('path');
@@ -15,95 +16,96 @@ const PORT = process.env.PORT || '8080';
 
 loaders.push({
   test: /\.scss$/,
-  loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?sourceMap&minimize&localIdentName=[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded' }),
+  loader: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader?sourceMap&minimize!postcss-loader!sass-loader?outputStyle=expanded',
+  }),
   exclude: ['node_modules'],
 });
 
+
 module.exports = {
-    entry: {
-        'app':
+  entry: {
+    app:
+      [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        './src/index.jsx',
+      ],
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, 'public'),
+    filename: 'assets/js/[name].bundle.js',
+    chunkFilename: 'assets/js/[name].chunk.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      scss: path.resolve(__dirname, 'scss/'),
+      vendors: path.resolve(__dirname, 'static/vendors/'),
+    },
+  },
+  module: {
+    loaders,
+  },
+  devServer: {
+    contentBase: './public',
+    noInfo: true,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST,
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['app'],
+      minChunks: ({ resource }) => /node_modules/.test(resource),
+    }),
+    new BundleAnalyzerPlugin(),
+    new ExtractTextPlugin({
+      disable: process.env.NODE_ENV === 'development',
+      filename: 'assets/css/[name].[contenthash].css',
+      allChunks: true,
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new DashboardPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'static', to: 'assets' },
+    ], {
+      ignore:
         [
-            'babel-polyfill',
-            'react-hot-loader/patch',
-            './src/index.jsx',
-        ]
-    },
-    devtool: 'cheap-module-source-map',
-    output: {
-        publicPath: '/',
-        path: path.join(__dirname, 'public'),
-        filename: 'assets/js/[name].bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            scss: path.resolve(__dirname, 'src/scss/'),
-            vendors: path.resolve(__dirname, 'static/vendors/'),
-        },
-    },
-    module: {
-        loaders
-    },
-    devServer: {
-        contentBase: './public',
-        noInfo: true,
-        hot: true,
-        inline: true,
-        historyApiFallback: true,
-        port: PORT,
-        host: HOST
-    },
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default'],
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'vendor',
-          chunks: ['app'],
-          minChunks: ({ resource }) => /node_modules/.test(resource),
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'lib',
-          chunks: ['app'],
-          minChunks: ({ resource }) => /src\/app\/lib\/vendor/.test(resource),
-        }),
-        new BundleAnalyzerPlugin(),
-        new ExtractTextPlugin({
-            filename: 'assets/css/main.css',
-            allChunks: true
-        }),
-        new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-        new DashboardPlugin(),
-        new CopyWebpackPlugin([
-            { from: 'static', to: 'assets' },
-        ], {
-            ignore:
-                [
-                    '*.css',
-                    '*.scss',
-                    '*.sass',
-                ],
-            copyUnmodified: false
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.hbs',
-            minify: {
-                collapseWhitespace: true,
-                preserveLineBreaks: true
-            },
-        }),
-        /*new HtmlWebpackIncludeAssetsPlugin({
+          '*.css',
+          '*.scss',
+          '*.sass',
+        ],
+      copyUnmodified: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.hbs',
+      minify: {
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+      },
+    }),
+    /* new HtmlWebpackIncludeAssetsPlugin({
             assets: [
                 'js/file.js',
             ],
             append: true,
         }), */
-    ]
+  ],
 };
--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -11,104 +11,109 @@ const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plug
 
 loaders.push({
   test: /\.scss$/,
-  loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?sourceMap&minimize&localIdentName=[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded' }),
+  loader: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader?sourceMap&minimize!postcss-loader!sass-loader?outputStyle=expanded',
+  }),
   exclude: ['node_modules'],
 });
 
+
 module.exports = {
-    entry: {
-        'app':
+  entry: {
+    app:
+    [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      './src/index.jsx',
+    ],
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    publicPath: './',
+    path: path.join(__dirname, 'public'),
+    filename: 'assets/js/[name].bundle.[hash].js',
+    chunkFilename: 'assets/js/[name].[hash].[chunkhash].chunk.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      static: path.resolve(__dirname, './static'),
+    },
+  },
+  module: {
+    loaders,
+  },
+  plugins: [
+    new WebpackCleanupPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi],
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['app'],
+      minChunks: ({ resource }) => /node_modules/.test(resource),
+    }),
+    new ExtractTextPlugin({
+      disable: process.env.NODE_ENV === 'development',
+      filename: 'assets/css/[name].[contenthash].css',
+      allChunks: true,
+    }),
+    new CopyWebpackPlugin([
+      { from: 'static', to: 'assets' },
+    ], {
+      ignore:
             [
-                'babel-polyfill',
-                'react-hot-loader/patch',
-                './src/index.jsx',
-            ]
-    },
-    devtool: 'cheap-module-source-map',
-    output: {
-        publicPath: './',
-        path: path.join(__dirname, 'public'),
-        filename: 'assets/js/[name].bundle.[chunkhash].js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            scss: path.resolve(__dirname, 'src/scss/'),
-            vendors: path.resolve(__dirname, 'static/vendors/'),
-        },
-    },
-    module: {
-        loaders
-    },
-    plugins: [
-        new WebpackCleanupPlugin(),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default'],
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: true,
-            compress: {
-                warnings: false,
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true
-            },
-            output: {
-                comments: false,
-            },
-            exclude: [/\.min\.js$/gi]
-        }),
-        new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            chunks: ['app'],
-            minChunks: ({resource}) => /node_modules/.test(resource)
-        }),
-        new ExtractTextPlugin({
-            filename: 'assets/css/main.css',
-            allChunks: true
-        }),
-        new CopyWebpackPlugin([
-            { from: 'static', to: 'assets' },
-        ], {
-            ignore:
-                [
-                    '*.css',
-                    '*.scss',
-                    '*.sass',
-                ],
-            copyUnmodified: false
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.hbs',
-            minify: {
-                collapseWhitespace: true,
-                preserveLineBreaks: false
-            },
-        }),
-        /*new HtmlWebpackIncludeAssetsPlugin({
-            assets: [
-                'js/file.js',
+              '*.css',
+              '*.scss',
+              '*.sass',
             ],
-            append: true,
-        }), */
-        new CompressionPlugin({
-            asset: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: /\.js$|\.css$|\.html$/,
-            threshold: 10240,
-            minRatio: 0
-        })
-    ]
+      copyUnmodified: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.hbs',
+      minify: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+      },
+    }),
+    /* new HtmlWebpackIncludeAssetsPlugin({
+        assets: [
+            'js/file.js',
+        ],
+        append: true,
+    }), */
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0,
+    }),
+  ],
 };
