3770d80ba5bb05b6932da06cb3ce85f40e35bd44
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-12-04 08:53:59 -0200

    fixed bug regarding bootstrap version and async loading

--- src/app/hoc/loadable.jsx
+++ src/app/hoc/loadable.jsx
@@ -1,7 +1,7 @@
 import L from 'react-loadable';
 import LoadingPage from '../component/loading-page';
 
-const Loadable = opts => L({
+const Loadable = (opts) => L({
   loading: LoadingPage,
   delay: 300,
   ...opts,
