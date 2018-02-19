f553579d69a26e88de064fbe3d6bd2f4fd9c85e7
Marcos Gonçalves <marcos.goncalves@wimdu.com>
2018-02-19 11:35:39 +0100

    :zap: added async function demo

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const App = () => {
+const App = (event) => {
+  const handleButtonClick = async () => {
+    document.querySelector('body').style.backgroundColor = '#117bf3';
+    document.querySelector('body').style.color = '#ffffff';
+  };
+
   return (
     <div>
       <Helmet>
         <title>React Redux Starter Pack</title>
       </Helmet>
-      App works!
+      App works!<br />
+      <button onClick={handleButtonClick}>Async button</button>
     </div>
   );
 };
